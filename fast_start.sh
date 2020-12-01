6378a0abf4cec430dc4c142136fc33985a90dd5f
Тютеньков Никита Борисович <n.tyutenkov@efko.ru>
2020-12-01 14:58:44 +0300

    yes conda

--- fast_start.sh
+++ fast_start.sh
@@ -27,7 +27,7 @@ curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > 
 sudo install -o root -g root -m 644 conda.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" | sudo tee /etc/apt/sources.list.d/conda.list
 sudo apt update
-sudo apt install conda
+sudo apt install -y conda
 sudo ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
 sudo mkdir /opt/conda/envs/
 sudo /opt/conda/bin/conda create --prefix /opt/conda/envs/python python=3.8 ipykernel
