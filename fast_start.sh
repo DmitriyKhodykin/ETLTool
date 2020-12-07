f59c10113a0f33ad9ae5fcd3655354d2dca43ed4
Nikita-efko <n.tyutenkov@efko.ru>
2020-12-07 15:13:53 +0300

    Поправлен файл установки + добавлен postgre

--- fast_start.sh
+++ fast_start.sh
@@ -7,11 +7,10 @@ sudo apt install -y nodejs
 
 # JupyterHub + JupyterLab
 sudo apt install -y python3-pip
+sudo python3 -m pip install -y papermill
 sudo apt install -y python3-venv
 sudo python3 -m venv /opt/jupyterhub/
-sudo /opt/jupyterhub/bin/python3 -m pip install wheel
-sudo /opt/jupyterhub/bin/python3 -m pip install jupyterhub jupyterlab
-sudo /opt/jupyterhub/bin/python3 -m pip install ipywidgets
+sudo /opt/jupyterhub/bin/python3 -m pip install wheel jupyterhub jupyterlab ipywidgets
 sudo mkdir -p /opt/jupyterhub/etc/jupyterhub/
 cp jupyterhub/jupyterhub_config.py /opt/jupyterhub/etc/jupyterhub/jupyterhub_config.py
 sudo mkdir -p /opt/jupyterhub/etc/systemd
@@ -20,12 +19,22 @@ npm install -g configurable-http-proxy
 sudo ln -s /opt/jupyterhub/etc/systemd/jupyterhub.service /etc/systemd/system/jupyterhub.service
 sudo systemctl daemon-reload
 sudo systemctl enable jupyterhub.service
+
+# Conda
+curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > conda.gpg
+sudo install -o root -g root -m 644 conda.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" | sudo tee /etc/apt/sources.list.d/conda.list
+sudo apt update
+sudo apt install -y conda
+sudo ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+sudo mkdir /opt/conda/envs/
+sudo /opt/conda/bin/conda create --prefix /opt/conda/envs/python python=3.8 ipykernel
+sudo /opt/conda/envs/python/bin/python -m ipykernel install --prefix=/opt/jupyterhub/ --name 'python' --display-name "Python (default)"
+
 sudo systemctl start jupyterhub.service
 
-# Papermill, pandas, requests, petl
-sudo /opt/jupyterhub/bin/python3 -m pip install papermill
-sudo /opt/jupyterhub/bin/python3 -m pip install petl
-sudo /opt/jupyterhub/bin/python3 -m pip install pandas
+# petl, pandas
+sudo /opt/jupyterhub/bin/python3 -m pip install petl pandas
 
 # Cronicle
 curl -s https://raw.githubusercontent.com/jhuckaby/Cronicle/master/bin/install.js | node
@@ -35,14 +44,24 @@ systemctl daemon-reload
 systemctl enable cronicle.service
 systemctl start cronicle.service
 
+sudo ufw allow 3012/tcp
+
 # NGINX
 sudo apt install -y nginx
 rm /etc/nginx/sites-enabled/default
 cp nginx/default.conf /etc/nginx/conf.d/default.conf
 service nginx restart
+sudo ufw allow 'Nginx HTTP'
 
 # Шаблон для корневой ссылки
 cp -r html /var/www
 
+# PostgreSQL
+sudo apt install -y postgresql postgresql-contrib
+cp -f postgresql/postgresql.conf /etc/postgresql/12/main/
+cp -f postgresql/pg_hba.conf /etc/postgresql/12/main/
+sudo ufw allow postgresql/tcp
+sudo ufw allow 5432/tcp
+service postgresql restart
 
 
